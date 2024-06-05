ee1a99b72757ac42abffb4e6cbd85615484aabad
Usman <usmanmamatkulov4.com>
2024-06-06 00:58:52 +0600

    n

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -145,5 +145,7 @@ SOCIALACCOUNT_PROVIDERS = { }
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
